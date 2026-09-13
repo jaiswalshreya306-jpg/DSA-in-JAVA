2df35765731f13b6bf33aefe1e7ebba1988fcc38
Shreya Jaiswal <jaiswalshreya306@gmail.com>
2026-09-13 18:10:07 +0000

    Welcome to dsa journey

---
+++ pattern1.java
@@ -0,0 +1,19 @@
+public class pattern1{
+    static void main(){
+
+        int n = 8;
+        for (int row = 1; row<=n; row++){
+   // for each rows --- n column
+   for(int col=1;col<=n;col++){
+//print star
+
+System.out.print("* ");
+   }
+   // move to next line or row
+   System.out.println();
+}
+    }
+}
+
+    
+    
