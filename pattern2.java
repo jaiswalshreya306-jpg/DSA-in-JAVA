d48c455c936b844de7db1fc9fa34ea03fcbf0056
Shreya Jaiswal <jaiswalshreya306@gmail.com>
2026-09-14 14:45:38 +0000

    Dream. Work. Achieve

---
+++ pattern2.java
@@ -0,0 +1,13 @@
+public class pattern2{
+    static void main(){
+         int n = 6;
+         for( int row=1; row<=n; row++){
+         for(int col=1;col<=5;col++){
+         System.out.print("* ");
+         }
+         System.out.println();
+
+    }
+}
+        }
+
